d21032a094fe9c02c5649b85f2e15fbf55683708
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-12-07 20:59:27 +0300

    migrate to consulo descr
    drop dead call

--- src/test/java/com/dmarcotte/handlebars/util/HbTestUtils.java
+++ src/test/java/com/dmarcotte/handlebars/util/HbTestUtils.java
@@ -1,9 +1,5 @@
 package com.dmarcotte.handlebars.util;
 
-import com.intellij.openapi.application.PathManager;
-
-import java.io.File;
-
 public class HbTestUtils {
   /**
    * The root of the test data directory
@@ -11,10 +7,6 @@ public class HbTestUtils {
   public static final String BASE_TEST_DATA_PATH = findTestDataPath();
 
   private static String findTestDataPath() {
-    File f = new File("test", "data");
-    if (f.exists()) {
-      return f.getAbsolutePath();
-    }
-    return PathManager.getHomePath() + "/contrib/handlebars/test/data";
+    return "";
   }
 }
